11a59b3111a1b36d2503e28ed4316836039af583
@satyadevi-nyros <satyadevi_nyros@yahoo.com>
2016-01-25 15:27:23 +0530

    Initial commit

--- payments/tests.py
+++ payments/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import *
+
+# Create your tests here.
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+    	print "hai"
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.failUnlessEqual(1 + 1, 2)
+
+    def test(self):
+    	print "testing"
+    	self.failUnlessEqual(1+1,2)
+
+'''
+Run test case by using follwoing commands
+
+python manage.py test
+
+python manage.py test scrapcars.tests.CartradeTest.test_cartrade
+
+
+'''
+
